451b6c3fbb51461ca76079b85222ebb690259dd6
Nnamdi Odozi <nnamdi.odozi@yahoo.com>
2025-03-29 17:25:52 +0000

    Updated init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,18 @@
 -- Create the user
-DO
-$$
-BEGIN
-    IF NOT EXISTS (
-        SELECT FROM pg_catalog.pg_roles
-        WHERE rolname = 'mlx_user'
-    ) THEN
-        CREATE ROLE mlx_user WITH LOGIN PASSWORD DB_PASSWORD;
-    END IF;
-END
-$$;
+-- DO
+-- $$
+-- BEGIN
+--     IF NOT EXISTS (
+--         SELECT FROM pg_catalog.pg_roles
+--         WHERE rolname = 'mlx_user'
+--     ) THEN
+--         CREATE ROLE mlx_user WITH LOGIN PASSWORD DB_PASSWORD;
+--     END IF;
+-- END
+-- $$;
 
--- Create the database (if using a separate one)
-CREATE DATABASE mlx_db OWNER mlx_user;
+-- -- Create the database (if using a separate one)
+-- CREATE DATABASE mlx_db OWNER mlx_user;
 
 -- Connect to that database to create tables (only works manually; not inside init.sql)
 -- So instead, create the table in the default DB
