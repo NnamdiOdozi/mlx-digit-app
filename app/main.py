dc99fe207af0195e2b6879c3d2ef0f427f5a5aca
Nnamdi Odozi <nnamdi.odozi@yahoo.com>
2025-03-29 19:46:02 +0000

    main.py, testing my github CI

--- app/main.py
+++ app/main.py
@@ -24,6 +24,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+# Testing my github CI
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
